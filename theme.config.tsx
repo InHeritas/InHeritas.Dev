2ef4798ea3734a686499108b3ed1369bf460b3cc
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2023-12-23 08:42:11 +0900

    Fix scripts in Head

--- theme.config.tsx
+++ theme.config.tsx
@@ -18,11 +18,6 @@ const config: DocsThemeConfig = {
   },
   head: (
     <>
-      <Script 
-        src="https://ryj9d86xjh.execute-api.ap-northeast-2.amazonaws.com/v1/api/fontstream/djs/?sid=gAAAAABkdUt-7O7BL6uR0fYqvxMIchs6KNCQVUp924pT2YaH9_V3IMQtHqHN72hAPhIqooO5hT2s2VROhCrzUfBlPyrN-ORbmgo6MJqCkSB0ipc9at_rg6s9AwXVJVU9ZFA7hco6wwBB4AIwX_2q_rsSDipqvIBKpNqgTWmQq1GV8qlteBEGUYot5ldhjEGfc-PcftnzfBjYIjKT1ICTOsoGmCP5kqYpk-boQU7mvc8Eos5pvV_AH1FEqyzx6FvMmRHrEWqHrziu"
-        type="module"
-        strategy="beforeInteractive"
-      />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     </>
   ),
