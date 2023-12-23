59a40ca2d3c8514ebcf66f81bf5564e0575d473a
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2023-12-23 17:39:37 +0900

    Apply Vercel Speed Insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '/public/styles.css'
 config.autoAddCss = false
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 };
