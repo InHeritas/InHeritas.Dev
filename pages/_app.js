c7091e48c15ed2f0a446edfc6f5b8033a2af0202
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2023-12-23 17:35:03 +0900

    Apply Vercel Web Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import { config } from '@fortawesome/fontawesome-svg-core'
+import { Analytics } from '@vercel/analytics/react';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '/public/styles.css'
 config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 };
