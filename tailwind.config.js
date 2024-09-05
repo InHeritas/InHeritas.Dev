876b497fe08b8e4f50c5dea2e18ee3718dc6b325
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2024-09-05 17:57:29 +0900

    Rewrite release notes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: '0.9rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 }
