83ecb004c3d2e9f149fc1a0edb592ac521616542
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2024-09-05 17:27:40 +0900

    Remove sandoll web font

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script type="module" src="https://ryj9d86xjh.execute-api.ap-northeast-2.amazonaws.com/v1/api/fontstream/djs/?sid=gAAAAABkdUt-7O7BL6uR0fYqvxMIchs6KNCQVUp924pT2YaH9_V3IMQtHqHN72hAPhIqooO5hT2s2VROhCrzUfBlPyrN-ORbmgo6MJqCkSB0ipc9at_rg6s9AwXVJVU9ZFA7hco6wwBB4AIwX_2q_rsSDipqvIBKpNqgTWmQq1GV8qlteBEGUYot5ldhjEGfc-PcftnzfBjYIjKT1ICTOsoGmCP5kqYpk-boQU7mvc8Eos5pvV_AH1FEqyzx6FvMmRHrEWqHrziu" />
         </Head>
         <body>
           <Main />
